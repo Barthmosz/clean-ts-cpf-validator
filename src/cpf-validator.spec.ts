1b9d4cb93b91fd21f89081fcc6ee58e18c54c851
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-03 18:52:15 -0300

    test: ensure CpfValidator returns false if an invalid cpf is provided

---
+++ src/cpf-validator.spec.ts
@@ -0,0 +1,14 @@
+import { CpfValidator } from './cpf-validator'
+import { type Validator } from './cpf-protocol'
+
+const makeSut = (): Validator => {
+    const sut = CpfValidator
+    return sut
+}
+
+describe('Cpf Validator', () => {
+    test('should return false if an invalid cpf is provided', () => {
+        const sut = makeSut()
+        expect(sut.validate('123.456.789')).toBe(false)
+    })
+})
