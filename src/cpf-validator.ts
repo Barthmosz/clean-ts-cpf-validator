24f7475510509168104f9bc0547ee828898c4b34
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-03 18:55:25 -0300

    docs: add jsdocs to CpfValidator

--- src/cpf-validator.ts
+++ src/cpf-validator.ts
@@ -1,4 +1,9 @@
 export class CpfValidator {
+    /**
+     * Validates a cpf.
+     * @param {string} cpf
+     * @returns {boolean}
+    */
     static validate(cpf: string): boolean {
         const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
         return cpfRegex.test(cpf)
